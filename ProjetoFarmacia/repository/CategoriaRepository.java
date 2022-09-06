309a85cdbe40ee67f1c9fa7f8faa880929aad884
Vinicius lopes <105940878+viniciuslopes98@users.noreply.github.com>
2022-09-06 10:40:29 -0300

    aula 05-09-22

---
+++ ProjetoFarmacia/repository/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package com.ProjetoFarmacia.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.ProjetoFarmacia.model.Categoria;
+
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+	public List <Categoria> findAllByTarjaContainingIgnoreCase(String tarja);
+}
